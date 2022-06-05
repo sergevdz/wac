e1ca02158d3c7fdf29d694dc4d5b7534a346e097
Sergio Valdez <sergio@wasp.mx>
2022-06-05 00:20:46 -0500

    Agregar datos chat room

--- app/server/controllers/chatRoom.js
+++ app/server/controllers/chatRoom.js
@@ -1,7 +1,33 @@
+// utils
+import makeValidation from "@withvoid/make-validation"
+import ChatRoomModel from "../models/chat-room.js"
+
 export default {
-    initiate: async (req, res) => { },
-    postMessage: async (req, res) => { },
-    getRecentConversation: async (req, res) => { },
-    getConversationByRoomId: async (req, res) => { },
-    markConversationReadByRoomId: async (req, res) => { },
-  }
+  initiate: async (req, res) => {
+    try {
+      const validation = makeValidation(types => ({
+        payload: req.body,
+        checks: {
+          userIds: {
+            type: types.array,
+            options: { unique: true, empty: false, stringOnly: true }
+          },
+          type: { type: types.enum, options: { enum: CHAT_ROOM_TYPES } },
+        }
+      }));
+      if (!validation.success) return res.status(400).json({ ...validation });
+
+      const { userIds, type } = req.body;
+      const { userId: chatInitiator } = req;
+      const allUserIds = [...userIds, chatInitiator];
+      const chatRoom = await ChatRoomModel.initiateChat(allUserIds, type, chatInitiator);
+      return res.status(200).json({ success: true, chatRoom });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error })
+    }
+  },
+  postMessage: async (req, res) => { },
+  getRecentConversation: async (req, res) => { },
+  getConversationByRoomId: async (req, res) => { },
+  markConversationReadByRoomId: async (req, res) => { },
+}
--- app/server/index.js
+++ app/server/index.js
@@ -1,7 +1,15 @@
-import http from "http";
-import express from "express";
-import logger from "morgan";
-import cors from "cors";
+// import http from "http";
+const http = require("http");
+// import express from "express";
+const express = require("express");
+// import logger from "morgan";
+const { logger } = require("morgan")
+// import cors from "cors";
+const cors = require("cors");
+
+// import * as socketio from 'socket.io'
+const socketio = require("socket.io");
+// mongo
 import "../config/mongo.js";
 
 // routes
@@ -9,8 +17,11 @@ import indexRouter from "./routes/index.js";
 import userRouter from "./routes/user.js";
 import chatRoomRouter from "./routes/chatRoom.js";
 import deleteRouter from "./routes/delete.js";
+
 // middlewares
 import { decode } from './middlewares/jwt.js'
+
+import websockets from "./utils/websockets.js";
 
 const app = express();
 
@@ -43,3 +54,7 @@ server.listen(port);
 server.on("listening", () => {
   console.log(`Listening on port:: http://localhost:${port}/`)
 });
+
+/** Create socket connection */
+global.io = socketio.listen(server);
+global.io.on('connection', websockets.connection);
